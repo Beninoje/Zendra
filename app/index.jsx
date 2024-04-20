2a6297e42fcc3ca9662b73e806eb2fe7724f7519
Beninoje <beninoje@hotmail.com>
2024-04-20 09:35:28 -0400

    added Focus button tab functionality

--- app/index.jsx
+++ app/index.jsx
@@ -2,13 +2,15 @@ import { Text, View } from "react-native";
 import { Link } from 'expo-router';
 import { useFonts } from 'expo-font';
 
-
+//* when using routing in React Native, in your folder you MUST have '_layout' 
+//* and if route to any file in that folder, it will bring automatically to '_layout' file
 export default function Page() {
   
   return (
        <View className="flex-1 items-center justify-center bg-white">
         <Text className="text-3xl font-pblack">Zendra</Text>
-        <Link href="/profile" className="text-blue-500 text-lg"> Go to Profile</Link>
+
+        <Link href="/home" className="text-blue-500 text-lg"> Go to Home</Link>
       </View>
   );
 }
